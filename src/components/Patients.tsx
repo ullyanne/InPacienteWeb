5ec20470521b0596924d19bc88626e6a99a5dd09
Ullyanne <ujfp@ic.ufal.br>
2024-05-03 18:35:01 -0300

    use searchbar component

--- src/components/Patients.tsx
+++ src/components/Patients.tsx
@@ -1,8 +1,8 @@
 import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { DataTable } from "./DataTable";
 import { PatientsColumns } from "./PatientsColumns";
+import { SearchBar } from "./SearchBar";
 
 export function Patients() {
   let data = []
@@ -35,13 +35,10 @@ export function Patients() {
       </div>
 
       <div className="mb-2 flex justify-between w-[1000px] pb-4">
-        <div className="relative">
-          <FontAwesomeIcon className="absolute inset-y-2.5 start-0 ps-3 pointer-events-none rtl:inset-r-0" icon={faMagnifyingGlass} />
-          <input type="text" className="block p-2 ps-9 text-sm text-gray-900 border border-gray-300 rounded-lg w-sm bg-gray-50" placeholder="Pesquisar..." />
-        </div>
+        <SearchBar />
 
         <div className="flex flex-row-reverse w-[1000px]">
-          <button className="bg-teal-200 border border-gray-300 rounded-lg py-2 px-2 hover:bg-teal-300/80 transition ease-in-out select-none delay-75">
+          <button className="bg-teal-200 border border-gray-300 rounded-lg py-2 px-2 hover:bg-teal-300/80 transition ease-in-out select-none delay-75 flex">
             <FontAwesomeIcon icon={faUserPlus} fontSize={"1.3em"} />
             <span className="pl-2">Novo paciente</span>
           </button>
