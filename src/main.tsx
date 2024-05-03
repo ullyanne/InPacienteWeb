ab942a0e92ba8e3cd82ec63dabc868ed7a4decde
Ullyanne <ujfp@ic.ufal.br>
2024-05-03 14:01:04 -0300

    create react browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import NotFoundPage from './components/NotFoundPage.tsx'
+import { Patients } from './components/Patients.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      {
+        index: true,
+        element: <Patients />
+      },
+      {
+        path: '/consultas',
+        element: <div>Consultas</div>
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
