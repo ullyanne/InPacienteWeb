ad08b4f71d027f4c57f5944dde2d8f49fce00165
Ullyanne <ujfp@ic.ufal.br>
2024-05-13 20:19:27 -0300

    🦺 add cpf and phone number mask in patients create and edit page

--- src/components/PatientsForm.tsx
+++ src/components/PatientsForm.tsx
@@ -1,6 +1,7 @@
-import { SubmitHandler, useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 import { usePatientsApi } from "../api/patients/PatientsApi";
+import { IMaskInput } from "react-imask";
 
 export type PatientFormFields = {
   name: string
@@ -19,15 +20,23 @@ interface PatientsFormProps {
 
 export function PatientsForm({ pageTitle, buttonTitle, patientCpf, isEditForm, onSubmit }: PatientsFormProps) {
 
-  const { register, handleSubmit, reset, formState: { errors, dirtyFields } } = useForm<PatientFormFields>()
+  const { register, handleSubmit, reset, control, formState: { errors, dirtyFields } } = useForm<PatientFormFields>(
+    {
+      defaultValues: {
+        phoneNumber: null
+      }
+    }
+  )
 
   const patientsAPI = usePatientsApi()
+  const [phone, setPhone] = useState('')
 
   useEffect(() => {
     if (isEditForm) {
       (async () => {
         try {
           const response = await patientsAPI.getPatientData(patientCpf)
+          setPhone(response?.phoneNumber!)
           reset(response)
         }
         catch (e) {
@@ -61,26 +70,49 @@ export function PatientsForm({ pageTitle, buttonTitle, patientCpf, isEditForm, o
           <div className="flex gap-6">
             <div>
               <span className="block text-sm font-medium text-slate-700">CPF</span>
-              <input {...register("cpf", {
-                required: isEditForm ? "" : "CPF é obrigatório",
-                pattern: /^[\d+$]/
-              })}
-                type="cpf"
-                disabled={isEditForm}
-                className="mt-1 block w-40 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-teal-300 focus:ring-1 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 
-                disabled:cursor-not-allowed"
+              <Controller
+                name="cpf"
+                control={control}
+                key={patientCpf}
+                rules={{
+                  required: isEditForm ? "" : "CPF do paciente é obrigatório"
+                }}
+                render={({ field }) => (
+                  <IMaskInput
+                    mask="000.000.000-00"
+                    defaultValue={isEditForm ? patientCpf : ""}
+                    unmask={true}
+                    disabled={isEditForm}
+                    onAccept={(value) => field.onChange(value)}
+
+                    className="mt-1 block w-40 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-teal-300 focus:ring-1 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 
+                  disabled:cursor-not-allowed"
+                  />
+
+                )}
               />
               {errors.cpf && <div className="text-red-500 text-xs mt-1">{errors.cpf.message}</div>}
             </div>
 
             <div>
               <span className="block text-sm font-medium text-slate-700">Número de telefone</span>
-              <input {...register("phoneNumber", {
-                maxLength: 11,
-                setValueAs: (value) => value === "" ? null : value
-              })}
-                type="phoneNumber"
-                className="mt-1 block w-[200px] px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-teal-300 focus:ring-1 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+              
+              <Controller
+                name="phoneNumber"
+                control={control}
+                key={phone}
+                render={({ field }) => (
+                  <IMaskInput
+                    mask="(00) 00000-0000"
+                    defaultValue={isEditForm ? phone : ""}
+                    unmask={true}
+                    onAccept={(value) => field.onChange(value)}
+
+                    className="mt-1 block w-40 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-teal-300 focus:ring-1 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 
+                  disabled:cursor-not-allowed"
+                  />
+
+                )}
               />
             </div>
           </div>
